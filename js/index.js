dcf17d5f97085ab2b810df339ca17113d0e87a8b
Fabian Lopez <fabianlopezmagnan@gmail.com>
2022-12-10 17:52:50 -0300

    correccion

--- js/index.js
+++ js/index.js
@@ -12,15 +12,19 @@ function imprimirJSONes(){
 
         // Obtiene un elemento del documento HTML Párrafo
         var p = document.getElementById('textoP');
-
-        // Establece el contenido del archivo JSON como el texto del párrafo
-        p.textContent = json.es.prueba1+"<br>"+json.es.prueba2;
+        var p2 = document.getElementById('textoP2');
 
         // Obtiene un elemento del documento HTML donde se quiere mostrar el párrafo
         var container = document.getElementById('container');
 
+        // Establece el contenido del archivo JSON como el texto del párrafo
+        p.textContent = json.es.prueba1;
+        p.textContent = json.es.prueba2;
+        
+
         // Agrega el párrafo al elemento
         container.appendChild(p);
+        container.appendChild(p2);
     });
 }
 function imprimirJSONen(){
@@ -35,14 +39,18 @@ function imprimirJSONen(){
 
         // Obtiene un elemento del documento HTML Párrafo
         var p = document.getElementById('textoP');
-
-        // Establece el contenido del archivo JSON como el texto del párrafo
-        p.textContent = json.en.prueba1+"<br>"+json.en.prueba2;
+        var p2 = document.getElementById('textoP2');
 
         // Obtiene un elemento del documento HTML donde se quiere mostrar el párrafo
         var container = document.getElementById('container');
 
+        // Establece el contenido del archivo JSON como el texto del párrafo
+        p.textContent = json.en.prueba1;
+        p.textContent = json.en.prueba2;
+        
+
         // Agrega el párrafo al elemento
         container.appendChild(p);
+        container.appendChild(p2);
     });
 }
