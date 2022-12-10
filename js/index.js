1e0e5735086fef81d0c587c8139d008aeb046cfd
Fabian Lopez <fabianlopezmagnan@gmail.com>
2022-12-10 17:47:06 -0300

    correccion de idiomas prueba

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-document.getElementById('jsonBTNes').addEventListener('click',imprimirJSON);
-
-function imprimirJSON(){
+document.getElementById('jsonBTNes').addEventListener('click',imprimirJSONes);
+document.getElementById('jsonBTNes').addEventListener('click',imprimirJSONen);
+function imprimirJSONes(){
     fetch('js/info.json')
     .then(function(res){
         // Devuelve el contenido del archivo JSON parseado como un objeto JavaScript
@@ -10,11 +10,34 @@ function imprimirJSON(){
         // Accede al contenido del archivo JSON
         console.log(json);
 
-        // Crea un elemento p
-        var p = document.createElement('p');
+        // Obtiene un elemento del documento HTML Párrafo
+        var p = document.getElementById('textoP');
 
         // Establece el contenido del archivo JSON como el texto del párrafo
-        p.textContent = json.es.prueba1;
+        p.textContent = json.es.prueba1+"<br>"+json.es.prueba2;
+
+        // Obtiene un elemento del documento HTML donde se quiere mostrar el párrafo
+        var container = document.getElementById('container');
+
+        // Agrega el párrafo al elemento
+        container.appendChild(p);
+    });
+}
+function imprimirJSONen(){
+    fetch('js/info.json')
+    .then(function(res){
+        // Devuelve el contenido del archivo JSON parseado como un objeto JavaScript
+        return res.json();
+    })
+    .then(function(json){
+        // Accede al contenido del archivo JSON
+        console.log(json);
+
+        // Obtiene un elemento del documento HTML Párrafo
+        var p = document.getElementById('textoP');
+
+        // Establece el contenido del archivo JSON como el texto del párrafo
+        p.textContent = json.en.prueba1+"<br>"+json.en.prueba2;
 
         // Obtiene un elemento del documento HTML donde se quiere mostrar el párrafo
         var container = document.getElementById('container');
