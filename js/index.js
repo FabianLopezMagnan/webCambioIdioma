0210898724c69e03d1616c59fd916694f115fc89
Fabian Lopez <fabianlopezmagnan@gmail.com>
2022-12-10 17:18:51 -0300

    correccion

--- js/index.js
+++ js/index.js
@@ -1,16 +1,5 @@
 document.getElementById('jsonBTN').addEventListener('click',imprimirJSON);
 
-function cargarJSON(){
-    fetch('js/info.json')
-    .then(function(res){
-        // Devuelve el contenido del archivo JSON parseado como un objeto JavaScript
-        return res.json();
-    })
-    .then(function(json){
-        // Accede al contenido del archivo JSON
-        console.log(json);
-    });
-}
 function imprimirJSON(){
     fetch('js/info.json')
     .then(function(res){
