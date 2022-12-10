6c24b727ad734b88f786dab768db1c78bf29581c
Fabian Lopez <fabianlopezmagnan@gmail.com>
2022-12-10 18:11:42 -0300

    correccion

--- js/index.js
+++ js/index.js
@@ -1,42 +1,32 @@
-document.getElementById('jsonBTNes').addEventListener('click', imprimirJSONes);
-document.getElementById('jsonBTNes').addEventListener('click', imprimirJSONen);
-
-function imprimirJSONes() {
-  fetch('js/info.json')
-    .then(function(res) {
-      // Devuelve el contenido del archivo JSON parseado como un objeto JavaScript
-      return res.json();
-    })
-    .then(function(json) {
-      // Accede al contenido del archivo JSON
-      console.log(json);
-
-      // Obtiene un elemento del documento HTML Párrafo
-      var p = document.getElementById('textoP');
-      var p2 = document.getElementById('textoP2');
-
-      // Establece el contenido del archivo JSON como el texto del párrafo
-      p.textContent = json.es.prueba1;
-      p2.textContent = json.es.prueba2;
-    });
-}
-
-function imprimirJSONen() {
-  fetch('js/info.json')
-    .then(function(res) {
-      // Devuelve el contenido del archivo JSON parseado como un objeto JavaScript
-      return res.json();
-    })
-    .then(function(json) {
-      // Accede al contenido del archivo JSON
-      console.log(json);
-
-      // Obtiene un elemento del documento HTML Párrafo
-      var p = document.getElementById('textoP');
-      var p2 = document.getElementById('textoP2');
-
-      // Establece el contenido del archivo JSON como el texto del párrafo
-      p.textContent = json.en.prueba1;
-      p2.textContent = json.en.prueba2;
-    });
-}
+function obtenerJSON() {
+    fetch('js/info.json')
+      .then(function(res) {
+        // Devuelve el contenido del archivo JSON parseado como un objeto JavaScript
+        return res.json();
+      })
+      .then(function(json) {
+        // Accede al contenido del archivo JSON
+        console.log(json);
+        return json;
+      });
+  }
+  
+  document.getElementById('jsonBTNes').addEventListener('click', function() {
+    var json = obtenerJSON();
+    var p = document.getElementById('textoP');
+    var p2 = document.getElementById('textoP2');
+    p.textContent = json.es.prueba1;
+    p2.textContent = json.es.prueba2;
+  });
+  
+  document.getElementById('jsonBTNen').addEventListener('click', function() {
+    var json = obtenerJSON();
+    var p = document.getElementById('textoP');
+    var p2 = document.getElementById('textoP2');
+    p.textContent = json.en.prueba1;
+    p2.textContent = json.en.prueba2;
+  });
+  
+  
+  
+  
