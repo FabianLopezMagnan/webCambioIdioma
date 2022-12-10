f779cf77424f3fa5b695becef6806d2d9d8d98eb
Fabian Lopez <fabianlopezmagnan@gmail.com>
2022-12-10 19:00:23 -0300

    correccion

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
-document.getElementById('jsonBTNes').addEventListener('click',imprimirJSONEspañol);
-//document.getElementById('jsonBTNes').addEventListener('click',imprimirJSONen);
-function imprimirJSONEspañol(){
+document.getElementById('jsonBTNes').addEventListener('click',español);
+document.getElementById('jsonBTNen').addEventListener('click',ingles);
+function español(){
+    console.log("Español");
     fetch('js/info.json')
     .then(function(res){
         // Devuelve el contenido del archivo JSON parseado como un objeto JavaScript
@@ -13,6 +14,22 @@ function imprimirJSONEspañol(){
         // Establece el contenido del archivo JSON como el texto del párrafo
         p.textContent = json.es.prueba1;
         p2.textContent = json.es.prueba2;
+    });
+}
+function ingles(){
+    console.log("INGLES");
+    fetch('js/info.json')
+    .then(function(res){
+        // Devuelve el contenido del archivo JSON parseado como un objeto JavaScript
+        return res.json();
+    })
+    .then(function(json){
+        // Obtiene un elemento del documento HTML Párrafo
+        var p = document.getElementById('textoP');
+        var p2 = document.getElementById('textoP2');
+        // Establece el contenido del archivo JSON como el texto del párrafo
+        p.textContent = json.en.prueba1;
+        p2.textContent = json.en.prueba2;
     });
 }
 
