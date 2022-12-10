9e727019c5c3ec9b29b5171bc4179c65ad3cac90
Fabian Lopez <fabianlopezmagnan@gmail.com>
2022-12-10 16:43:59 -0300

    arreglo de codigo

--- js/index.js
+++ js/index.js
@@ -1,9 +1,13 @@
 document.getElementById('jsonBTN').addEventListener('click',cargarJSON);
 
-
 function cargarJSON(){
     fetch('js/info.json')
     .then(function(res){
-        console.log(res);
+        // Devuelve el contenido del archivo JSON parseado como un objeto JavaScript
+        return res.json();
     })
+    .then(function(json){
+        // Accede al contenido del archivo JSON
+        console.log(json);
+    });
 }
