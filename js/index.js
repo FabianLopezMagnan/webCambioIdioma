a60ce864ed08a7f09274c95f14da1c12b1a0bee9
Fabian Lopez <fabianlopezmagnan@gmail.com>
2022-12-10 18:30:10 -0300

    correccion

--- js/index.js
+++ js/index.js
@@ -1,9 +1,5 @@
 function obtenerJSON() {
     fetch('js/info.json')
-      .then(function(res) {
-        // Devuelve el contenido del archivo JSON parseado como un objeto JavaScript
-        return res.json();
-      })
       .then(function(json) {
         // Accede al contenido del archivo JSON
         console.log(json);
@@ -11,11 +7,20 @@ function obtenerJSON() {
       });
   }
   document.getElementById('jsonBTNes').addEventListener('click', function() {
+    console.log("entre a funcion para Español");
     var json = obtenerJSON();
     var p = document.getElementById('textoP');
     var p2 = document.getElementById('textoP2');
     p.textContent = json.es.prueba1;
     p2.textContent = json.es.prueba2;
+  });
+  document.getElementById('jsonBTNen').addEventListener('click', function() {
+    console.log("entre a funcion para Ingles");
+    var json = obtenerJSON();
+    var p = document.getElementById('textoP');
+    var p2 = document.getElementById('textoP2');
+    p.textContent = json.en.prueba1;
+    p2.textContent = json.en.prueba2;
   });
   
   
