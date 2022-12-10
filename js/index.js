67953e659825520d0d10d8b7175bad3046464c7f
Fabian Lopez <fabianlopezmagnan@gmail.com>
2022-12-10 17:10:30 -0300

    imprimiendo json en HTML

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-document.getElementById('jsonBTN').addEventListener('click',cargarJSON);
+document.getElementById('jsonBTN').addEventListener('click',imprimirJSON);
 
 function cargarJSON(){
     fetch('js/info.json')
@@ -9,5 +9,28 @@ function cargarJSON(){
     .then(function(json){
         // Accede al contenido del archivo JSON
         console.log(json);
+    });
+}
+function imprimirJSON(){
+    fetch('js/info.json')
+    .then(function(res){
+        // Devuelve el contenido del archivo JSON parseado como un objeto JavaScript
+        return res.json();
+    })
+    .then(function(json){
+        // Accede al contenido del archivo JSON
+        console.log(json);
+
+        // Crea un elemento p
+        var p = document.createElement('p');
+
+        // Establece el contenido del archivo JSON como el texto del párrafo
+        p.textContent = json;
+
+        // Obtiene un elemento del documento HTML donde se quiere mostrar el párrafo
+        var container = document.getElementById('container');
+
+        // Agrega el párrafo al elemento
+        container.appendChild(p);
     });
 }
